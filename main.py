aaac0b884b077fd82acbfc445f3a59a5c3037f54
lszh02 <464575284@qq.com>
2024-05-30 17:34:33 +0800

    有摆药数据时，画图分析

--- main.py
+++ main.py
@@ -27,43 +27,48 @@ def process_excel(file_path):
     # 将“操作日期”列转换为日期格式
     df['操作日期'] = pd.to_datetime(df['操作日期']).dt.date
 
-    # 按照操作日期分组，并计算每日的最低库存量
-    daily_min_stock = df.groupby('操作日期')['库存量'].min().reset_index()
-    daily_min_stock.rename(columns={'库存量': '当日最低库存'}, inplace=True)
-
     # 筛选出类型为"住院摆药"的记录
-    hospitalized_medicine = df[df['类型'] == '住院摆药']
-    # 按照操作日期分组，并计算每日的入出库数量总和
-    daily_sales = hospitalized_medicine.groupby('操作日期')['入出库数量'].sum().abs().reset_index()
-    daily_sales.rename(columns={'入出库数量': '当日销量'}, inplace=True)
+    if '住院摆药' in df['类型'].values:
+        hospitalized_medicine = df[df['类型'] == '住院摆药']
 
-    # 合并每日最低库存和每日销量的数据
-    merged_df = pd.merge(daily_min_stock, daily_sales, on='操作日期', how='outer')
+        # 按照操作日期分组，并计算每日的入出库数量总和
+        daily_sales = hospitalized_medicine.groupby('操作日期')['入出库数量'].sum().abs().reset_index()
+        daily_sales.rename(columns={'入出库数量': '当日销量'}, inplace=True)
+        # 按照操作日期分组，并计算每日的最低库存量
+        daily_min_stock = hospitalized_medicine.groupby('操作日期')['库存量'].min().reset_index()
+        daily_min_stock.rename(columns={'库存量': '当日最低库存'}, inplace=True)
 
-    # 将 '操作日期' 列转换为日期时间格式
-    merged_df['操作日期'] = pd.to_datetime(merged_df['操作日期'])
+        # 合并每日最低库存和每日销量的数据
+        merged_df = pd.merge(daily_min_stock, daily_sales, on='操作日期', how='outer')
 
-    # 生成一个包含所有日期的序列，也是日期时间格式
-    all_dates = pd.date_range(start=merged_df['操作日期'].min(), end=merged_df['操作日期'].max())
+        # 将 '操作日期' 列转换为日期时间格式
+        merged_df['操作日期'] = pd.to_datetime(merged_df['操作日期'])
 
-    # 直接将 '操作日期' 列作为键进行合并，因为现在两者都是日期时间格式
-    merged_df = pd.merge(all_dates.to_frame(name='操作日期'), merged_df, on='操作日期', how='left')
+        # 生成一个包含所有日期的序列，也是日期时间格式
+        all_dates = pd.date_range(start=merged_df['操作日期'].min(), end=merged_df['操作日期'].max())
 
-    # 将日期时间格式的 '操作日期' 列转换为日期格式
-    merged_df['操作日期'] = merged_df['操作日期'].dt.date
+        # 直接将 '操作日期' 列作为键进行合并，因为现在两者都是日期时间格式
+        merged_df = pd.merge(all_dates.to_frame(name='操作日期'), merged_df, on='操作日期', how='left')
 
-    # 使用前一个有效值填充每日最低库存的缺失值
-    merged_df['当日最低库存'].fillna(method='ffill', inplace=True)
+        # 将日期时间格式的 '操作日期' 列转换为日期格式
+        merged_df['操作日期'] = merged_df['操作日期'].dt.date
 
-    # 使用0填充每日销量的缺失值
-    merged_df['当日销量'].fillna(0, inplace=True)
+        # 使用前一个有效值填充每日最低库存的缺失值
+        merged_df['当日最低库存'].fillna(method='ffill', inplace=True)
 
-    # 计算近7日、30日的日均销量
-    merged_df['近7日的日均销量'] = merged_df['当日销量'].rolling(window=7, min_periods=1).mean().round(2)
-    # merged_df['近30日的日均销量'] = merged_df['当日销量'].rolling(window=30, min_periods=1).mean().round(2)
+        # 使用0填充每日销量的缺失值
+        merged_df['当日销量'].fillna(0, inplace=True)
 
-    # 筛选出短缺记录
-    shortage_df = merged_df[merged_df['当日最低库存'] < merged_df['近7日的日均销量']].copy()
+        # 计算近7日、30日的日均销量
+        merged_df['近7日的日均销量'] = merged_df['当日销量'].rolling(window=7, min_periods=1).mean().round(2)
+        # merged_df['近30日的日均销量'] = merged_df['当日销量'].rolling(window=30, min_periods=1).mean().round(2)
+
+        # 筛选出短缺记录
+        shortage_df = merged_df[merged_df['当日最低库存'] < merged_df['近7日的日均销量']].copy()
+    else:
+        print("警告：选定周期内，'住院摆药'类型的记录不存在！")
+        shortage_df = pd.DataFrame()
+        merged_df = pd.DataFrame()
 
     if not shortage_df.empty:
         # 补充原始数据中的部分信息,以便输出到Excel中进行后续分析
