a4a731f2e70d0f6c66c5704ef10c1d796b597f9a
lszh02 <464575284@qq.com>
2024-12-01 10:09:36 +0800

    日志逐日备份

--- config.py
+++ config.py
@@ -5,15 +5,17 @@ directory_path = r'D:\药事\5.降低静配中心药品供应短缺率\消耗记
 export_path = r'D:\药事\5.降低静配中心药品供应短缺率\汇总记录'
 
 # 设置日志文件路径
-app_log_path = os.path.join(os.path.dirname(__file__), "app.log")
-error_log_path = os.path.join(os.path.dirname(__file__), "errors.log")
+app_log_path = os.path.join(os.path.dirname(__file__), "log/app.log")
+error_log_path = os.path.join(os.path.dirname(__file__), "log/errors.log")
 
-# 配置应用日志格式和输出
+# 配置应用日志格式和输出，逐日备份
 app_logger = loguru.logger
 app_logger.add(app_log_path, format="{time} | {level} | {message}",
-               filter=lambda record: record["level"].name == "INFO")
+               filter=lambda record: record["level"].name == "INFO" or record["level"].name == "WARNING",
+               rotation="1 day", retention="60 days")
 
-# 配置错误日志格式和输出
+# 配置错误日志格式和输出，逐日备份
 error_logger = loguru.logger
 error_logger.add(error_log_path, format="{time} | {level} | {message}",
-                 filter=lambda record: record["level"].name == "ERROR" or record["level"].name == "CRITICAL")
+                 filter=lambda record: record["level"].name == "ERROR" or record["level"].name == "CRITICAL",
+                 rotation="1 day", retention="60 days")
